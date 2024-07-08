835a6201b2364cf41ba1471ccaaf21a7d128737c
wnwlals13 <wnwlals13@naver.com>
2024-07-09 00:16:42 +0900

    feat: 240709 메인 반응형 수정

--- src/components/mypage/mypage.tsx
+++ src/components/mypage/mypage.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import styles from "./mypage.module.css";
 
 export default function Mypage() {
+  const handleEditProfile = () => {};
   return (
     <section className={styles.container}>
-      <div className={styles.profile}>
-        <div className={styles.profileImg}>img</div>
-        <div className={styles.profileComment}>comment</div>
-        <div className={styles.profileBtn}>
-          <button>설정</button>
+      <div className={styles.leftContainer}>
+        <div className={styles.profile}>
+          <div className={styles.profileImg}>
+            <input type="file" className={styles.profileBtn} />
+          </div>
+          <h2 className={styles.name}>name</h2>
+          <div className={styles.profileComment}>comment</div>
+          {/* <div className={styles.profileBtn}>
+            <button className={styles.myBtn}>설정</button>
+          </div> */}
         </div>
+        <div className={styles.tags}>list</div>
       </div>
       <div className={styles.postList}>
         <li>list1</li>
